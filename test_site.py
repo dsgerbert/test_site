fa3756291785f07f8c22e6d92baeed5acb558320
server tech login <h562@ovz1.dsgerbert.6el81.vps.myjino.ru>
2016-08-18 18:51:19 +0300

    bug fix

--- test_site.py
+++ test_site.py
@@ -3,7 +3,7 @@ from bottle import route, run, template, static_file, error
 
 BASE_DIR = os.path.dirname(__file__) + os.path.sep
 test_site_host = os.environ['test_site_host'] or 'localhost'
-test_site_port = os.environ['test_site_port'] or 8080
+test_site_port = int(os.environ['test_site_port']) or 8080
 
 @route('/')
 def index():
